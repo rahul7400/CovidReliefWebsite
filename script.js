ee1a259f1b84e30b66ffe19b7c55d7c58d9f794a
Rahul <macrocodes7400@gmail.com>
2021-04-23 13:34:56 +0530

    message

--- script.js
+++ script.js
@@ -4,13 +4,18 @@ function fn1(){
     var str3 = '';
     
 
-    var markedCheckbox = document.getElementsByName("check");  
-    for (var checkbox of markedCheckbox) {  
-    if (checkbox.checked)  {
-        str3 = str3.concat(checkbox.value+'+OR+');
+    if(str==""){
+        alert("Please enter your city name")
+    }else{
+        var markedCheckbox = document.getElementsByName("check");  
+        for (var checkbox of markedCheckbox) {  
+        if (checkbox.checked)  {
+            str3 = str3.concat(checkbox.value+'+OR+');
+        }
+        
+        }  
+        
+        window.open("https://twitter.com/search?q=verified+"+str+"+"+str3+"plasma"+str2)
     }
-    
-    }  
-    
-    window.open("https://twitter.com/search?q=verified+"+str+"+"+str3+"plasma"+str2)
+   
 }
